bd224e733200f6ba8f27858c8c7983b4e6870245
Alain Leon <xerebz@gmail.com>
2019-12-30 07:07:10 +0000

    getting cooled up

--- f_mode.cpp
+++ f_mode.cpp
@@ -1,6 +1,17 @@
-#include <rte_eal.h>
-#include <rte_mempool.h>
+#include <rte_ethdev.h>
+
+int hello(__attribute__((unused)) void *arg) {
+  return printf("hi");
+}
+
+int port_init() {
+  return printf("bye");
+}
 
 int main(int argc, char **argv) {
+  unsigned lcore_id, port_id;
   rte_eal_init(argc, argv);
+  RTE_ETH_FOREACH_DEV(port_id) {
+    port_init();
+  }
 }
